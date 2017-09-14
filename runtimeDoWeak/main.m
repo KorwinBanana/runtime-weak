acf395c0b59eeab74960a9bff295f87a9d79f2a0
korwin <523404234@qq.com>
2017-09-14 14:49:16 +0800

    Initial Commit

---
+++ runtimeDoWeak/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  runtimeDoWeak
+//
+//  Created by korwin on 2017/9/14.
+//  Copyright © 2017年 korwin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
